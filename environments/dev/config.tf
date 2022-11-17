c1c394c2e33391e884bb134deaf4eca5affa9d0c
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2022-11-17 23:32:03 +0100

    I-8 Use secret manager to store resources credentials (#9)

--- environments/dev/config.tf
+++ environments/dev/config.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    akeyless = {
+      source  = "akeyless-community/akeyless"
+      version = "1.1.15"
+    }
+  }
+}
+
+provider "akeyless" {
+  api_gateway_address = var.akeyless_api_gateway_address
+
+  api_key_login {
+    access_id  = var.akeyless_access_id
+    access_key = var.akeyless_access_key
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
---
+++ environments/dev/variables.tf
@@ -0,0 +1,12 @@
+variable "akeyless_api_gateway_address" {
+  type = string
+  default = "https://api.akeyless.io"
+}
+
+variable "akeyless_access_id" {
+  type = string
+}
+
+variable "akeyless_access_key" {
+  type = string
+}
